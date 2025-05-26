e813b33b8fe798d7b86f36771db29077e7dea7eb
ilarramendi <ilarramendi1@gmail.com>
2025-05-26 02:31:01 -0300

    Improved examples

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
-const translations = {
-	test: 'Hola Soy Un Test',
-	nested: {
-		nestedTest: 'Hola Soy Un Test Anidado',
-	},
-	withValue: {
-		hello: 'Hola {nombre}',
-	},
-	plural: {
-		example_none: 'No hay nada',
-		example_one: 'Hay un elemento',
-		withOtherValues_none: 'No hay nada y {nombre}',
-		withOtherValues_one: 'Hay {count} elemento y {nombre}',
-		withOtherValues_other: 'Hay {count} elementos y {nombre} o {apellido}',
-	}
-} as const;
+
 
 type RemovePluralSuffix<T extends string> = T extends `${infer Base}_none`
 	? Base
@@ -65,8 +50,6 @@ type ExtractPlaceholders<S> =
 	? Param | ExtractPlaceholders<Rest>
 	: never;
 
-type exampleExtractPlaceholders = ExtractPlaceholders<GetValue<typeof translations, 'plural.withOtherValues'>>;
-
 type InterpolationProps<S, Plural extends boolean> =
 	ExtractPlaceholders<S> extends never
 	? Plural extends true
@@ -80,68 +63,74 @@ type Simplify<T> = {
 	[K in keyof T]: T[K]
 } & {}
 
-export function translate<
-	Translations extends typeof translations,
-	Value extends GetValue<Translations, Key>,
-	Key extends DotNestedLeafKeys<Translations>,
-	Plural extends IsPlural<Translations, Key>,
->(
-	key: Key,
-	...args: InterpolationProps<Value, Plural> extends Record<string, never>
-		? []  // No params needed if no placeholders
-		: [params: Simplify<InterpolationProps<Value, Plural>>]  // Params required if placeholders exist
-): Value {
-	const parts = key.split('.');
-	let current = translations;
-	for (const part of parts) {
-		// @ts-expect-error
-		if (current[part]) {
-			// @ts-expect-error
-			current = current[part];
-		} else {
-			break;
-		}
-	}
 
-	if (typeof current === 'undefined') {
-		console.error(`Translation key "${key}" not found`);
-		return key as unknown as Value;
-	}
-
-	if (typeof current === 'object') {
-		// If its an object being returned check if its a plural key
-		// @ts-expect-error
-		const isPlural = current[`${parts.at(-1)}_none`] !== undefined || current[`${parts.at(-1)}_one`] !== undefined || current[`${parts.at(-1)}_other`] !== undefined;
-		if (isPlural) {
-			if (!args[0] || !Object.hasOwn(args[0], 'count')) {
-				console.error(`Missing count value for plural key "${key}"`);
-				return key as unknown as Value;
-			}
+export function getTranslate<
+	Translations
+>(translations: Translations) {
+	function translate<
+		Value extends GetValue<Translations, Key>,
+		Key extends DotNestedLeafKeys<Translations>,
+		Plural extends IsPlural<Translations, Key>,
+	>(
+		key: Key,
+		...args: InterpolationProps<Value, Plural> extends Record<string, never>
+			? []  // No params needed if no placeholders
+			: [params: Simplify<InterpolationProps<Value, Plural>>]  // Params required if placeholders exist
+	): Value {
+		const parts = key.split('.');
+		let current = translations;
+		for (const part of parts) {
 			// @ts-expect-error
-			const count = Number(args[0]?.count);
-			if (!count) {
+			if (current[part]) {
 				// @ts-expect-error
-				current = current[`${parts.at(-1)}_none`];
-			} else if (count === 1) {
-				// @ts-expect-error
-				current = current[`${parts.at(-1)}_one`];
+				current = current[part];
 			} else {
-				// @ts-expect-error
-				current = current[`${parts.at(-1)}_other`];
+				break;
 			}
-		} else {
-			console.error(`Incomplete translation key "${key}"`);
+		}
+
+		if (typeof current === 'undefined') {
+			console.error(`Translation key "${key}" not found`);
 			return key as unknown as Value;
 		}
-	}
 
-	let value = String(current);
-	const params = args[0];
-	if (params) {
-		for (const [param, val] of Object.entries(params)) {
-			value = value.replaceAll(`{${param}}`, String(val));
+		if (typeof current === 'object') {
+			// If its an object being returned check if its a plural key
+			// @ts-expect-error
+			const isPlural = current[`${parts.at(-1)}_none`] !== undefined || current[`${parts.at(-1)}_one`] !== undefined || current[`${parts.at(-1)}_other`] !== undefined;
+			if (isPlural) {
+				if (!args[0] || !Object.hasOwn(args[0], 'count')) {
+					console.error(`Missing count value for plural key "${key}"`);
+					return key as unknown as Value;
+				}
+				// @ts-expect-error
+				const count = Number(args[0]?.count);
+				if (!count) {
+					// @ts-expect-error
+					current = current[`${parts.at(-1)}_none`];
+				} else if (count === 1) {
+					// @ts-expect-error
+					current = current[`${parts.at(-1)}_one`];
+				} else {
+					// @ts-expect-error
+					current = current[`${parts.at(-1)}_other`];
+				}
+			} else {
+				console.error(`Incomplete translation key "${key}"`);
+				return key as unknown as Value;
+			}
 		}
+
+		let value = String(current);
+		const params = args[0];
+		if (params) {
+			for (const [param, val] of Object.entries(params)) {
+				value = value.replaceAll(`{${param}}`, String(val));
+			}
+		}
+
+		return value as Value;
 	}
 
-	return value as Value;
+	return translate;
 }
--- src/test.ts
+++ src/test.ts
@@ -1,55 +1,69 @@
-import { translate } from './index';
+import { getTranslate } from './index';
 
-// Normal translation
+const translations = {
+	test: 'Regular translation',
+	nested: {
+		test: 'Nested translation',
+	},
+	withValue: 'With value {value}',
+	// Key dosnt need to include plural, its just for the example
+	examplePlural_none: 'No elements',
+	examplePlural_one: 'One element',
+	examplePlural_other: '{count} elements',
+	examplePluralWithOtherValues_none: 'No elements and {name}',
+	examplePluralWithOtherValues_one: 'One element and {name}',
+	examplePluralWithOtherValues_other: '{count} elements and {name} or {name2}',
+} as const;
+
+const translate = getTranslate(translations);
+
+
+// Normal
 const translation = translate('test');
 //     ^?
 
-// Nested translation
-const translation2 = translate('nested.nestedTest');
+// Nested
+const translation2 = translate('nested.test');
 //    ^? string
 
-// Incorrect translation
+// Incorrect
 const translation3 = translate('nested.noFunciona');
+
+// With value
+const translation4 = translate('withValue', { value: 'Juan' });
 //     ^?
 
-// With value translation
-const translation4 = translate('withValue.hello', { nombre: 'Juan' });
-//     ^?
-
-// Incorrect value translation
-const translation5 = translate('withValue.hello', { incorrect: 'Juan' });
-//     ^?
-
-// Missing value key
-const translation6 = translate('withValue.hello', {});
-//     ^?
+// Incorrect value
+const translation5 = translate('withValue', { incorrect: 'Juan' });
 
 // Unnecesary value
-const translation7 = translate('nested.nestedTest', { nombre: 'Juan' });
-//     ^?
+const translation7 = translate('test', {});
 
-// Missing value
-const translation8 = translate('withValue.hello');
-//     ^?
+// Missing value key
+const translation6 = translate('withValue', {});
+
+// Missing value property
+const translation8 = translate('withValue');
 
 // Plural
-const translation9 = translate('plural.example', { count: 123 });
+const translation9 = translate('examplePlural', { count: 123 });
+//     ^?
 
 // Plural missing count value
-const translation10 = translate('plural.example', {});
+const translation10 = translate('examplePlural', {});
 
 // Plural missing values
-const translation11 = translate('plural.example');
+const translation11 = translate('examplePlural');
 
 // Plural with other values
-const translation12 = translate('plural.withOtherValues', { count: 0, nombre: 'Juan', apellido: 'Perez' });
+const translation12 = translate('examplePluralWithOtherValues', { count: 0, name: 'Juan', name2: 'Perez' });
 
 // Plural with other values missing count value
-const translation13 = translate('plural.withOtherValues', { nombre: 123 });
+const translation13 = translate('examplePluralWithOtherValues', { name: 'Juan', name2: 'Perez' });
 
 // Plural with other values missing values
-const translation14 = translate('plural.withOtherValues', { count: 123 });
+const translation14 = translate('examplePluralWithOtherValues', { count: 123 });
 
-// Plural with other values missing values
-const translation15 = translate('plural.withOtherValues');
+// Plural missing value property
+const translation15 = translate('examplePluralWithOtherValues');
 
